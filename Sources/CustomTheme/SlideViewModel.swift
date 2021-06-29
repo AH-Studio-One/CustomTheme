1ba739b468bdbae5249dc74cbd16299e3af14c2b
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-06-29 08:55:02 +0200

    Made default init public

--- Sources/CustomTheme/SlideViewModel.swift
+++ Sources/CustomTheme/SlideViewModel.swift
@@ -5,16 +5,30 @@
 //  Created by A H on 2021-06-28.
 //
 
-import Foundation
+import SwiftUI
 
 public struct SlideViewModel {
     let title: String
     let subTitle:String
     let theme: Theme
 
+    public init() {
+        self.title = "title"
+        self.subTitle = "subTitle"
+        self.theme = SampleTheme()
+    }
+    
     public init(title: String, subTitle:String, theme: Theme) {
         self.title = title
         self.subTitle = subTitle
         self.theme = theme
     }
+}
+
+struct SampleTheme: Theme {
+    var primaryColor = Color.red
+    var secondaryColor = Color.yellow
+    var bgColor = Color.gray
+    var primaryFont = Font.largeTitle
+    var secondaryFont = Font.footnote
 }
